c51c23e08babe2b632f7c013bbfd06b016c913d6
eventlog <szymko931@gmail.com>
2019-03-15 15:23:51 +0100

    test

--- logonuser.ps1
+++ logonuser.ps1
@@ -1,1 +1,2 @@
-invoke-command -computer <computername> -scriptblock {Get-EventLog -logname system -newest 5 |select -Property username, timegenerated}
+invoke-command -computer [computername] -scriptblock {Get-EventLog -logname system -newest 5 |select -Property username, timegenerated}
+//test
