edbcc8d020271d1de1927c46937ac26ac0819611
eventlog <szymko931@gmail.com>
2019-03-19 10:13:48 +0100

    :)

--- BOUtils.ps1
+++ BOUtils.ps1
@@ -15,7 +15,7 @@ do
      Show-Menu
      $input = Read-Host "DOKONAJ WYBORU..."
      switch ($input)
-     {
+    {
            '1'  {
                 cls
                 get-service -name *BO* -computername serwer-bo1, serwer-bo2 | Where-Object {$_.Status -eq "Running"}
@@ -24,7 +24,7 @@ do
                 Get-Process -ComputerName serwer-bo1, serwer-bo2 | Where-Object {($_.processName -match 'sia') -or ($_.processName -match 'tomcat8') -or ($_.processName -match 'CMS')}
            }'3' {
                cls
-                Write-Host „Podaj uzytkownika + $user”
+                $user = Read-Host "Podaj uzytkownika"
                 $users = $user
                 $group = '#FL_BO'
                 $members = Get-ADGroupMember -Identity $group -Recursive | Select-Object -ExpandProperty SAMAccountName
@@ -32,20 +32,35 @@ do
                 {
                 If ($members -contains $user) 
                 {
-                Write-Host „$user nalezy do grupy $group”
+                    Write-Host "$user nalezy do grupy $group"
                 } 
                 Else 
                 {
-                Write-Host „$user NIE nalezy do grupy $group”
-            }
-            }
-           }
-           'q' {
+                    Write-Host "$user NIE nalezy do grupy $group"
+                    $zmienna = Read-Host "Czy chcesz go teraz dodac? tak/nie"
+                    switch ($zmienna) 
+                    {
+                        'tak'
+                        {
+                                    ForEach ($Group in $Groups) 
+                                    {
+                                            Add-ADPrincipalGroupMembership $User  -MemberOf  $Group   
+                                    }
+                            Write-Host "Uzytkownik został dodany do grupy!"
+                        }
+                        'nie'
+                        {
+                            Write-Host "Powodzenia dalej!"
+                        } 
+                    }
+                }
+                }   
+                }
+                'q' 
+           {
                 return
            }
-     }
+    }
      pause
 }
 until ($input -eq 'q')
-
-
