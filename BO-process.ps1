8923583c5cd036fc994a7b7a3236fd50bd210bb3
eventlog <szymko931@gmail.com>
2019-04-01 10:59:11 +0200

    BOProcess

--- BO-process.ps1
+++ BO-process.ps1
@@ -1,10 +1,13 @@
-function Get-BOProcess {
-    $serwer = Read-Host "Podaj serwer"
-    "sia","tomcat8","CMS" | foreach-object {
-    Get-Process -ComputerName $serwer -name $_ -PipelineVariable pv | 
-    Measure-Object Workingset -sum -average | 
+Clear-Host
+Function Get-BOProcess {
+    Param([string[]]$Name,[string]$Computername = $env:computername)
+
+    Invoke-Command -ScriptBlock {
+    $using:Name | foreach-object {
+    Get-Process -name $_ -PipelineVariable pv | 
+    Measure-Object Workingset -sum | 
     Select-object @{Name="Name";Expression = {$pv.name}},
-    Count,
-    @{Name="SumMB";Expression = {[math]::round($_.Sum/1MB,2)}}
+    @{Name="SumMB";Expression = {[math]::round($_.Sum/1MB,2)}} | Format-Table
     }
+    } -ComputerName $computername
     }
